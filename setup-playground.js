841f40c5db3bbff03fa76de24dab70d34d29ddfe
WIlliam Porto <william.porto@nave.rs>
2021-06-25 12:07:57 -0300

    🐛 (playground): setting theme based on html

--- setup-playground.js
+++ setup-playground.js
@@ -1,12 +1,30 @@
 export default (() => {
-  const initSnackPlayers = () => {
+  const setSnackColorScheme = () => {
+    const theme = document.querySelector("html").getAttribute("data-theme");
+    document.querySelectorAll(".snack-player").forEach((playground) => {
+      playground.setAttribute("data-snack-theme", theme);
+      window.ExpoSnack && window.ExpoSnack.remove(playground);
+      window.ExpoSnack && window.ExpoSnack.append(playground);
+    });
+  };
+
+  const initializeSnack = () => {
+    setSnackColorScheme();
     window.ExpoSnack && window.ExpoSnack.initialize();
-  }
+  };
+
+  new MutationObserver(() => setSnackColorScheme()).observe(
+    document.querySelector("html"),
+    {
+      attributes: true,
+      attributeFilter: ["data-theme"],
+    }
+  );
 
   return {
     onRouteUpdate() {
       setTimeout(() => {
-        initSnackPlayers()
+        initializeSnack();
       }, 0);
     },
   };
